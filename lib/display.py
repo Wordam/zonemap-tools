5c890cd347db675e20c0a5eb3257ace2dfdf032a
Wordam <niamor123@gmail.com>
2017-12-04 18:28:04 +0100

    Added lib folder

---
+++ lib/display.py
@@ -0,0 +1,177 @@
+"""Where all display method goes !."""
+
+import cv2
+import numpy as np
+import shapely.geometry as sg
+import matplotlib.pyplot as plt
+
+__color_map__ = {}
+__color_map__['False alarm'] = (43, 243, 240)
+__color_map__['Miss'] = (125, 125, 125)
+__color_map__['Match'] = (90, 239, 73)
+__color_map__['Merge'] = (255, 34, 255)
+__color_map__['Split'] = (239, 131, 73)
+__color_map__['Multiple'] = (228, 31, 31)
+
+def draw_polygon(img, polygon, color):
+    """Draw a shapely polygon."""
+    print(polygon)
+    if (isinstance(polygon, sg.collection.GeometryCollection)
+            or isinstance(polygon, sg.multipolygon.MultiPolygon)):
+        for geom in polygon.geoms:
+            if not isinstance(geom,sg.LineString):
+                draw_polygon(img, geom, color)
+    else:
+        if isinstance(polygon,list):
+            for geom in polygon:
+                draw_polygon(img, geom, color)
+        else:
+            pts = np.array(polygon.exterior.coords)
+            pts = np.int32([pts])
+            # cv2.polylines(img, pts, True, color, 1)
+            cv2.fillPoly(img, pts, color, 1)
+
+def display_matches(matches, img_path, alpha=0.4):
+    """Display errors groups."""
+    img = cv2.imread(img_path)
+    img_cpy = img.copy()
+    img_match = img.copy()
+    img_miss = img.copy()
+    img_false_alarm = img.copy()
+    img_split = img.copy()
+    img_merge = img.copy()
+    img_multiple = img.copy()
+
+    for _, match in matches.items():
+        # Draw miss(es)
+        if match['error_class'] == 'Miss':
+            draw_polygon(img_cpy, match['zone'], __color_map__["Miss"])
+            draw_polygon(img_miss, match['zone'], __color_map__["Miss"])
+
+        # Draw false alarm(s)
+        if match['error_class'] == 'False alarm':
+            draw_polygon(img_cpy, match['zone'], __color_map__["False alarm"])
+            draw_polygon(img_false_alarm, match['zone'], __color_map__["False alarm"])
+
+        # Draw splits
+        if match['error_class'] == 'Split':
+            draw_polygon(img_cpy, match['zone'], __color_map__["Split"])
+            draw_polygon(img_split, match['zone'], __color_map__["Split"])
+
+        # Draw merges
+        if match['error_class'] == 'Merge':
+            draw_polygon(img_cpy, match['zone'], __color_map__["Merge"])
+            draw_polygon(img_merge, match['zone'], __color_map__["Merge"])
+
+        # Draw match
+        if match['error_class'] == 'Match':
+            draw_polygon(img_cpy, match['zone'], __color_map__["Match"])
+            draw_polygon(img_match, match['zone'], __color_map__["Match"])
+
+        # Draw multiple
+        if match['error_class'] == 'Multiple':
+            draw_polygon(img_cpy, match['zone'], __color_map__["Match"])
+            draw_polygon(img_multiple, match['zone'], __color_map__["Match"])
+
+    cpy = img.copy()
+    cv2.addWeighted(img_match, alpha, cpy, 1 - alpha, 0, cpy)
+    cv2.imwrite("out/match.png", cpy)
+
+    cpy = img.copy()
+    cv2.addWeighted(img_miss, alpha, cpy, 1 - alpha, 0, cpy)
+    cv2.imwrite("out/miss.png", cpy)
+
+    cpy = img.copy()
+    cv2.addWeighted(img_false_alarm, alpha, cpy, 1 - alpha, 0, cpy)
+    cv2.imwrite("out/false_alarm.png", cpy)
+
+    cpy = img.copy()
+    cv2.addWeighted(img_split, alpha, cpy, 1 - alpha, 0, cpy)
+    cv2.imwrite("out/split.png", cpy)
+
+    cpy = img.copy()
+    cv2.addWeighted(img_merge, alpha, cpy, 1 - alpha, 0, cpy)
+    cv2.imwrite("out/merge.png", cpy)
+
+    cpy = img.copy()
+    cv2.addWeighted(img_multiple, alpha, cpy, 1 - alpha, 0, cpy)
+    cv2.imwrite("out/multiple.png", cpy)
+
+def display_errors(groups, img_path, alpha=0.4):
+    """Display errors groups."""
+    img = cv2.imread(img_path)
+    img_cpy = img.copy()
+    img_match = img.copy()
+    img_miss = img.copy()
+    img_false_alarm = img.copy()
+    img_split = img.copy()
+    img_merge = img.copy()
+
+    for group in groups:
+        details = group['error_details']
+        # Draw miss(es)
+        misses = details['miss']
+        if misses is not None:
+            for miss in misses:
+                draw_polygon(img_cpy, miss, __color_map__["Miss"])
+                draw_polygon(img_miss, miss, __color_map__["Miss"])
+
+        # Draw false alarm(s)
+        false_alarms = details['false_alarm']
+        if false_alarms is not None:
+            for false_alarm in false_alarms:
+                draw_polygon(img_cpy, false_alarm, __color_map__["False alarm"])
+                draw_polygon(img_false_alarm, false_alarm, __color_map__["False alarm"])
+
+        # Draw splits
+        splits = details['split']
+        if splits is not None:
+            for split in splits:
+                draw_polygon(img_cpy, split, __color_map__["Split"])
+                draw_polygon(img_split, split, __color_map__["Split"])
+
+        # Draw merges
+        merges = details['merge']
+        if merges is not None:
+            for merge in merges:
+                draw_polygon(img_cpy, merge, __color_map__["Merge"])
+                draw_polygon(img_merge, merge, __color_map__["Merge"])
+
+        # Draw match
+        match_poly = details['match']
+        if match_poly is not None:
+            draw_polygon(img_cpy, match_poly, __color_map__["Match"])
+            draw_polygon(img_match, match_poly, __color_map__["Match"])
+
+    cpy = img.copy()
+    cv2.addWeighted(img_match, alpha, cpy, 1 - alpha, 0, cpy)
+    cv2.imwrite("out/match.png", cpy)
+
+    cpy = img.copy()
+    cv2.addWeighted(img_miss, alpha, cpy, 1 - alpha, 0, cpy)
+    cv2.imwrite("out/miss.png", cpy)
+
+    cpy = img.copy()
+    cv2.addWeighted(img_false_alarm, alpha, cpy, 1 - alpha, 0, cpy)
+    cv2.imwrite("out/false_alarm.png", cpy)
+
+    cpy = img.copy()
+    cv2.addWeighted(img_split, alpha, cpy, 1 - alpha, 0, cpy)
+    cv2.imwrite("out/split.png", cpy)
+
+    cpy = img.copy()
+    cv2.addWeighted(img_merge, alpha, cpy, 1 - alpha, 0, cpy)
+    cv2.imwrite("out/merge.png", cpy)
+
+def display_graph(it_vect, datas):
+    ax = plt.subplot(111, xlabel='β', ylabel='Number of class error')
+    for item in ([ax.title, ax.xaxis.label, ax.yaxis.label] +
+                 ax.get_xticklabels() + ax.get_yticklabels()):
+        item.set_fontsize(35)
+
+    for name,data in datas.items():
+        plt.plot(it_vect, data, label=name, linewidth=7.0)
+    leg = plt.legend(loc='upper left', shadow=True, fancybox=True, prop={'size':35})
+    leg.get_frame().set_alpha(0.3)
+    plt.show()
+    plt.savefig('out.png')
---
+++ lib/utils.py
@@ -0,0 +1,82 @@
+"""All utility function not really related to ZoneMap."""
+
+import random
+import os
+from os.path import basename
+from os import listdir
+from collections import defaultdict
+import xml.etree.ElementTree as ET
+from collections import defaultdict
+from xml.etree.ElementTree import Element, SubElement, tostring
+from shapely.geometry import Polygon
+
+__red__, __green__, __blue__ = (255, 0, 0), (0, 255, 0), (0, 0, 255)
+__rgb__ = [__red__, __green__, __blue__]
+
+def get_random_color():
+    """Return a random rgb color."""
+    return __rgb__[random.randint(0, 2)]
+
+def zones_from_file(path):
+    """Parse zones from file."""
+    zones = {}
+    fil = open(path, 'r')
+    for line in fil:
+        splits = line.split(',')
+        zone_id = int(splits[0])
+        left, top, width, height = int(splits[1]), int(splits[2]), int(splits[3]), int(splits[4])
+        bottom = top+height
+        right = left + width
+        zones[zone_id] = Polygon([[left, top], [right, top], [right, bottom], [left, bottom]])
+    return zones
+
+def zones_from_gedi_xml(xml_path, gedi_type="Area"):
+    """Parse zones from a GEDI xml."""
+    tree = ET.parse(xml_path)
+    root = tree.getroot()
+    document = root.find('.//{http://lamp.cfar.umd.edu/media/projects/GEDI/}DL_DOCUMENT')
+    page = document.find('.//{http://lamp.cfar.umd.edu/media/projects/GEDI/}DL_PAGE')
+    zones = {}
+    for child in page:
+        if child.attrib['gedi_type'] == gedi_type:
+            zone_id = int(child.attrib['id'])
+            left = int(child.attrib['col'])
+            top = int(child.attrib['row'])
+            right = left + int(child.attrib['width'])
+            bottom = top + int(child.attrib['height'])
+            zones[zone_id] = Polygon([[left, top], [right, top], [right, bottom], [left, bottom]])
+    return zones
+
+def square(value):
+    """Return the square value."""
+    return value*value
+
+def xmls_from_folder(ref_folder, hyp_folder):
+    """Return matching pair of xml files from ref and hyp folders."""
+    file_pairs = []
+    for ref_file in listdir(ref_folder):
+        for hyp_file in listdir(hyp_folder):
+            if basename(ref_file) == basename(hyp_file):
+                file_pairs.append({'ref_file':'{}/{}'.format(ref_folder, ref_file),
+                                   'hyp_file':'{}/{}'.format(hyp_folder, hyp_file)})
+    return file_pairs
+
+def dsum(*dicts):
+    """Return the sum of dict by keys."""
+    ret = defaultdict(float)
+    for dictt in dicts:
+        for key, value in dictt.items():
+            ret[key] += value
+    return dict(ret)
+
+def daverage(*dicts):
+    """Return the sum of dict by keys."""
+    ret = defaultdict(float)
+    for dictt in dicts:
+        for key, value in dictt.items():
+            ret[key] += value
+            ret[key] /= 2
+    return dict(ret)
+
+def get_filename(path):
+    return os.path.splitext(path)[0]
